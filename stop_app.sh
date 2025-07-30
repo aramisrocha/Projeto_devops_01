1ecdedd3903b28b87f43412eb8d8b3ccb114dbe3
Aramis de Oliveira Andrade Rocha <aramis.rocha@ativas.com.br>
2025-07-29 21:52:50 -0300

    Ajustando o shell script

--- stop_app.sh
+++ stop_app.sh
@@ -3,9 +3,9 @@ echo "Parando aplicação Flask..."
 sudo pkill -f "python3 app.py"
 
 BACKUP_DIR="/opt/flaskapp_backup/$(date +%Y%m%d_%H%M%S)"
-mkdir -p "$BACKUP_DIR"
+sudo mkdir -p "$BACKUP_DIR"
 
 # Move tudo de /opt/flaskapp para o diretório de backup
-mv /opt/flaskapp/* "$BACKUP_DIR" 2>/dev/null || echo "Nada para mover"
+sudo mv /opt/flaskapp/* "$BACKUP_DIR" 2>/dev/null || echo "Nada para mover"
 
-echo "Backup realizado em: $BACKUP_DIR"
+sudo echo "Backup realizado em: $BACKUP_DIR"
